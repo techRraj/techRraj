7904ab1aad1fa42632e8186547ac931f5d7cdf9a
techRraj <143004057+techRraj@users.noreply.github.com>
2023-11-28 12:23:44 +0530

    Your commit message

---
+++ abc.php
@@ -0,0 +1,19 @@
+<?php 
+ echo "yrgj";
+
+ $a=56;
+ $b= 78;
+ $str="hio there";
+ $name="Raj";
+ $d= $a+$b;
+
+ echo "<br>sum of".$a." and ".$b;
+ echo "<br>sum = ".$d;
+
+ echo "<br>string example".$str;
+
+ echo "<br> <h1>hi there</h1>";
+?>
+
+<h2 style="color:red;text-align:center;background:#000;"><?php echo $name;?></h2>
+
